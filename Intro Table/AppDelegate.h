0cb6a0dfbc0aa6efa420f35c629290eb593fe0a4
xax <xax@xaxs-mac.local>
2016-06-20 20:56:18 -0500

    Initial Commit

---
+++ Intro Table/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Intro Table
+//
+//  Created by xax on 6/20/16.
+//  Copyright © 2016 xax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Intro Table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Intro Table
+//
+//  Created by xax on 6/20/16.
+//  Copyright © 2016 xax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
